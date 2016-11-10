95cd5ffdb890a4438bc95f24ce6fccca13d155e5
PlusL <ljh19950713@gmail.com>
2016-11-10 16:28:49 +0800

    Base64coding
    Try to learn some encrption. Here is a demo of base64 coding. The string
    structure performs like "a,b,c"

---
+++ EncriptTest/EncriptTest/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace EncriptTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileDisplayArea = new System.Windows.Forms.GroupBox();
+            this.TextBox_Phone = new System.Windows.Forms.TextBox();
+            this.TextBox_Company = new System.Windows.Forms.TextBox();
+            this.TextBox_Proposer = new System.Windows.Forms.TextBox();
+            this.TextBox_SeqNum = new System.Windows.Forms.TextBox();
+            this.Label_Phone = new System.Windows.Forms.Label();
+            this.Label_Company = new System.Windows.Forms.Label();
+            this.Label_Proposer = new System.Windows.Forms.Label();
+            this.Label_SeqNum = new System.Windows.Forms.Label();
+            this.FileSelectArea = new System.Windows.Forms.GroupBox();
+            this.Button_FilePath = new System.Windows.Forms.Button();
+            this.Lable_FilePath = new System.Windows.Forms.Label();
+            this.TextBox_FilePath = new System.Windows.Forms.TextBox();
+            this.RegArea = new System.Windows.Forms.GroupBox();
+            this.ComBox_RegTime = new System.Windows.Forms.ComboBox();
+            this.Label_RegTime = new System.Windows.Forms.Label();
+            this.Button_Reg = new System.Windows.Forms.Button();
+            this.Label_Result = new System.Windows.Forms.Label();
+            this.FileDisplayArea.SuspendLayout();
+            this.FileSelectArea.SuspendLayout();
+            this.RegArea.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FileDisplayArea
+            // 
+            this.FileDisplayArea.Controls.Add(this.TextBox_Phone);
+            this.FileDisplayArea.Controls.Add(this.TextBox_Company);
+            this.FileDisplayArea.Controls.Add(this.TextBox_Proposer);
+            this.FileDisplayArea.Controls.Add(this.TextBox_SeqNum);
+            this.FileDisplayArea.Controls.Add(this.Label_Phone);
+            this.FileDisplayArea.Controls.Add(this.Label_Company);
+            this.FileDisplayArea.Controls.Add(this.Label_Proposer);
+            this.FileDisplayArea.Controls.Add(this.Label_SeqNum);
+            this.FileDisplayArea.Font = new System.Drawing.Font("宋体", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FileDisplayArea.Location = new System.Drawing.Point(130, 80);
+            this.FileDisplayArea.Name = "FileDisplayArea";
+            this.FileDisplayArea.Size = new System.Drawing.Size(360, 174);
+            this.FileDisplayArea.TabIndex = 0;
+            this.FileDisplayArea.TabStop = false;
+            this.FileDisplayArea.Text = "文件信息";
+            // 
+            // TextBox_Phone
+            // 
+            this.TextBox_Phone.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TextBox_Phone.Location = new System.Drawing.Point(90, 135);
+            this.TextBox_Phone.Name = "TextBox_Phone";
+            this.TextBox_Phone.Size = new System.Drawing.Size(225, 21);
+            this.TextBox_Phone.TabIndex = 7;
+            // 
+            // TextBox_Company
+            // 
+            this.TextBox_Company.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TextBox_Company.Location = new System.Drawing.Point(90, 100);
+            this.TextBox_Company.Name = "TextBox_Company";
+            this.TextBox_Company.Size = new System.Drawing.Size(225, 21);
+            this.TextBox_Company.TabIndex = 6;
+            // 
+            // TextBox_Proposer
+            // 
+            this.TextBox_Proposer.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TextBox_Proposer.Location = new System.Drawing.Point(90, 65);
+            this.TextBox_Proposer.Name = "TextBox_Proposer";
+            this.TextBox_Proposer.Size = new System.Drawing.Size(225, 21);
+            this.TextBox_Proposer.TabIndex = 5;
+            // 
+            // TextBox_SeqNum
+            // 
+            this.TextBox_SeqNum.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TextBox_SeqNum.Location = new System.Drawing.Point(90, 30);
+            this.TextBox_SeqNum.Name = "TextBox_SeqNum";
+            this.TextBox_SeqNum.Size = new System.Drawing.Size(225, 21);
+            this.TextBox_SeqNum.TabIndex = 4;
+            // 
+            // Label_Phone
+            // 
+            this.Label_Phone.AutoSize = true;
+            this.Label_Phone.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_Phone.Location = new System.Drawing.Point(31, 135);
+            this.Label_Phone.Name = "Label_Phone";
+            this.Label_Phone.Size = new System.Drawing.Size(35, 14);
+            this.Label_Phone.TabIndex = 3;
+            this.Label_Phone.Text = "电话";
+            // 
+            // Label_Company
+            // 
+            this.Label_Company.AutoSize = true;
+            this.Label_Company.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_Company.Location = new System.Drawing.Point(31, 100);
+            this.Label_Company.Name = "Label_Company";
+            this.Label_Company.Size = new System.Drawing.Size(35, 14);
+            this.Label_Company.TabIndex = 2;
+            this.Label_Company.Text = "公司";
+            // 
+            // Label_Proposer
+            // 
+            this.Label_Proposer.AutoSize = true;
+            this.Label_Proposer.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_Proposer.Location = new System.Drawing.Point(31, 65);
+            this.Label_Proposer.Name = "Label_Proposer";
+            this.Label_Proposer.Size = new System.Drawing.Size(49, 14);
+            this.Label_Proposer.TabIndex = 1;
+            this.Label_Proposer.Text = "申请人";
+            // 
+            // Label_SeqNum
+            // 
+            this.Label_SeqNum.AutoSize = true;
+            this.Label_SeqNum.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_SeqNum.Location = new System.Drawing.Point(31, 30);
+            this.Label_SeqNum.Name = "Label_SeqNum";
+            this.Label_SeqNum.Size = new System.Drawing.Size(49, 14);
+            this.Label_SeqNum.TabIndex = 0;
+            this.Label_SeqNum.Text = "序列号";
+            this.Label_SeqNum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FileSelectArea
+            // 
+            this.FileSelectArea.Controls.Add(this.Button_FilePath);
+            this.FileSelectArea.Controls.Add(this.Lable_FilePath);
+            this.FileSelectArea.Controls.Add(this.TextBox_FilePath);
+            this.FileSelectArea.Font = new System.Drawing.Font("宋体", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FileSelectArea.Location = new System.Drawing.Point(130, 13);
+            this.FileSelectArea.Name = "FileSelectArea";
+            this.FileSelectArea.Size = new System.Drawing.Size(360, 48);
+            this.FileSelectArea.TabIndex = 1;
+            this.FileSelectArea.TabStop = false;
+            this.FileSelectArea.Text = "选择文件";
+            // 
+            // Button_FilePath
+            // 
+            this.Button_FilePath.Font = new System.Drawing.Font("宋体", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Button_FilePath.Location = new System.Drawing.Point(294, 14);
+            this.Button_FilePath.Name = "Button_FilePath";
+            this.Button_FilePath.Size = new System.Drawing.Size(51, 21);
+            this.Button_FilePath.TabIndex = 2;
+            this.Button_FilePath.Text = "浏览";
+            this.Button_FilePath.UseVisualStyleBackColor = true;
+            this.Button_FilePath.Click += new System.EventHandler(this.Button_FilePath_Click);
+            // 
+            // Lable_FilePath
+            // 
+            this.Lable_FilePath.AutoSize = true;
+            this.Lable_FilePath.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Lable_FilePath.Location = new System.Drawing.Point(29, 18);
+            this.Lable_FilePath.Name = "Lable_FilePath";
+            this.Lable_FilePath.Size = new System.Drawing.Size(53, 12);
+            this.Lable_FilePath.TabIndex = 1;
+            this.Lable_FilePath.Text = "文件路径";
+            // 
+            // TextBox_FilePath
+            // 
+            this.TextBox_FilePath.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBox_FilePath.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.TextBox_FilePath.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TextBox_FilePath.Location = new System.Drawing.Point(89, 15);
+            this.TextBox_FilePath.Name = "TextBox_FilePath";
+            this.TextBox_FilePath.ReadOnly = true;
+            this.TextBox_FilePath.Size = new System.Drawing.Size(198, 14);
+            this.TextBox_FilePath.TabIndex = 0;
+            this.TextBox_FilePath.MouseHover += new System.EventHandler(this.TextBox_FilePath_MouseHover);
+            // 
+            // RegArea
+            // 
+            this.RegArea.Controls.Add(this.ComBox_RegTime);
+            this.RegArea.Controls.Add(this.Label_RegTime);
+            this.RegArea.Font = new System.Drawing.Font("宋体", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.RegArea.Location = new System.Drawing.Point(130, 274);
+            this.RegArea.Name = "RegArea";
+            this.RegArea.Size = new System.Drawing.Size(360, 56);
+            this.RegArea.TabIndex = 2;
+            this.RegArea.TabStop = false;
+            this.RegArea.Text = "注册时间";
+            // 
+            // ComBox_RegTime
+            // 
+            this.ComBox_RegTime.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ComBox_RegTime.FormattingEnabled = true;
+            this.ComBox_RegTime.Items.AddRange(new object[] {
+            "15",
+            "30",
+            "90",
+            "180",
+            "360"});
+            this.ComBox_RegTime.Location = new System.Drawing.Point(91, 19);
+            this.ComBox_RegTime.Name = "ComBox_RegTime";
+            this.ComBox_RegTime.Size = new System.Drawing.Size(200, 24);
+            this.ComBox_RegTime.TabIndex = 1;
+            this.ComBox_RegTime.MouseEnter += new System.EventHandler(this.ComBox_RegTime_MouseEnter);
+            // 
+            // Label_RegTime
+            // 
+            this.Label_RegTime.AutoSize = true;
+            this.Label_RegTime.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_RegTime.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Label_RegTime.Location = new System.Drawing.Point(31, 28);
+            this.Label_RegTime.Name = "Label_RegTime";
+            this.Label_RegTime.Size = new System.Drawing.Size(53, 12);
+            this.Label_RegTime.TabIndex = 0;
+            this.Label_RegTime.Text = "注册时长";
+            // 
+            // Button_Reg
+            // 
+            this.Button_Reg.Location = new System.Drawing.Point(259, 347);
+            this.Button_Reg.Name = "Button_Reg";
+            this.Button_Reg.Size = new System.Drawing.Size(108, 48);
+            this.Button_Reg.TabIndex = 3;
+            this.Button_Reg.Text = "注册";
+            this.Button_Reg.UseVisualStyleBackColor = true;
+            this.Button_Reg.Click += new System.EventHandler(this.Button_Reg_Click);
+            // 
+            // Label_Result
+            // 
+            this.Label_Result.AutoSize = true;
+            this.Label_Result.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_Result.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Label_Result.Location = new System.Drawing.Point(285, 410);
+            this.Label_Result.Name = "Label_Result";
+            this.Label_Result.Size = new System.Drawing.Size(0, 16);
+            this.Label_Result.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.Label_Result);
+            this.Controls.Add(this.Button_Reg);
+            this.Controls.Add(this.RegArea);
+            this.Controls.Add(this.FileSelectArea);
+            this.Controls.Add(this.FileDisplayArea);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FileDisplayArea.ResumeLayout(false);
+            this.FileDisplayArea.PerformLayout();
+            this.FileSelectArea.ResumeLayout(false);
+            this.FileSelectArea.PerformLayout();
+            this.RegArea.ResumeLayout(false);
+            this.RegArea.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox FileDisplayArea;
+        private System.Windows.Forms.GroupBox FileSelectArea;
+        private System.Windows.Forms.GroupBox RegArea;
+        private System.Windows.Forms.Label Label_Phone;
+        private System.Windows.Forms.Label Label_Company;
+        private System.Windows.Forms.Label Label_Proposer;
+        private System.Windows.Forms.Label Label_SeqNum;
+        private System.Windows.Forms.Button Button_FilePath;
+        private System.Windows.Forms.Label Lable_FilePath;
+        private System.Windows.Forms.TextBox TextBox_FilePath;
+        private System.Windows.Forms.TextBox TextBox_Phone;
+        private System.Windows.Forms.TextBox TextBox_Company;
+        private System.Windows.Forms.TextBox TextBox_Proposer;
+        private System.Windows.Forms.TextBox TextBox_SeqNum;
+        private System.Windows.Forms.ComboBox ComBox_RegTime;
+        private System.Windows.Forms.Label Label_RegTime;
+        private System.Windows.Forms.Button Button_Reg;
+        private System.Windows.Forms.Label Label_Result;
+    }
+}
+
---
+++ EncriptTest/EncriptTest/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+
+
+namespace EncriptTest
+{
+    public partial class Form1 : Form
+    {
+//        Dictionary<string, TextBox> tb_seqnum = new Dictionary<string, TextBox>();
+//        Dictionary<string, TextBox> tb_proposer = new Dictionary<string, TextBox>();
+//        Dictionary<string, TextBox> tb_company = new Dictionary<string, TextBox>();
+//        Dictionary<string, TextBox> tb_phone = new Dictionary<string, TextBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ /*       private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                LoadAllSet();
+
+                this.CenterToScreen();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Form1_Load()" + ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+  */ 
+
+/*        private void LoadAllSet()
+        {
+            try
+            {
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("LoadAllSet() " + ex.Message, "Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+ */ 
+
+        public string EncodeBase64(Encoding code_type, string code)
+        {
+            string encode = "";
+            byte[] bytes = code_type.GetBytes(code);
+            try
+            {
+                encode = Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                encode = code;
+            }
+            return encode;
+        }
+        public string DecodeBase64(Encoding code_type, string code)
+        {
+            string decode = "";
+            byte[] bytes = Convert.FromBase64String(code);
+            try
+            {
+                decode = code_type.GetString(bytes);
+            }
+            catch
+            {
+                decode = code;
+            }
+            return decode;
+        }
+
+        private void Button_FilePath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                string file = ofd.FileName;
+                this.TextBox_FilePath.Text = file;
+            }
+            string Path = this.TextBox_FilePath.Text;
+            string Encodes = File.ReadAllText(Path,Encoding.UTF8);
+            string Decodes = DecodeBase64(Encoding.UTF8,Encodes);
+            string[] Decodes_list = Decodes.Split(',');
+            TextBox_SeqNum.Text = Decodes_list[0];
+            TextBox_Proposer.Text = Decodes_list[1];
+            TextBox_Company.Text = Decodes_list[2];
+            TextBox_Phone.Text = Decodes_list[3];
+        }
+
+        private void Button_Reg_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<string> codes = new List<string>();
+                codes.Add(TextBox_SeqNum.Text);
+                codes.Add(TextBox_Proposer.Text);
+                codes.Add(TextBox_Company.Text);
+                codes.Add(TextBox_Phone.Text);
+
+                long NowTime = DateTime.Now.Millisecond;
+                long time = NowTime + long.Parse(ComBox_RegTime.Text.ToString()) * 24L * 60L * 60L * 1000L + 8L * 60L * 60L * 1000L;
+                codes.Add(time.ToString());
+
+                string[] decodes = codes.ToArray();
+                string Decodes = string.Join(",", decodes);
+
+                string Encodes = EncodeBase64(Encoding.UTF8, Decodes);
+
+                SaveFileDialog sfd = new SaveFileDialog();
+
+                sfd.Filter = "key files (*.key)|*.txt|All Files (*.*)|*.*";
+                sfd.FilterIndex = 2;
+                sfd.RestoreDirectory = true;
+
+                if (sfd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                {
+                    StreamWriter sw = new StreamWriter(sfd.FileName);
+                    sw.Write(Encodes);
+                    sw.Dispose();
+                }
+                this.Label_Result.Text = "注册成功！";
+                this.Label_Result.ForeColor = Color.Green;
+            }
+            catch (Exception ex)
+            {
+                this.Label_Result.Text = "注册失败!";
+                this.Label_Result.ForeColor = Color.Red;
+                throw ex;
+            }
+
+            
+        }
+        private void ComBox_RegTime_MouseEnter(object sender, EventArgs e)
+        {
+            ToolTip TipInput = new ToolTip();
+
+            TipInput.AutoPopDelay = 5000;
+            TipInput.InitialDelay = 1000;
+            TipInput.ReshowDelay = 500;
+            TipInput.ShowAlways = true;
+
+            TipInput.SetToolTip(this.ComBox_RegTime, "请输入不小于0的整数，或从下方选择");
+            
+        }
+        private void TextBox_FilePath_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip TipShow = new ToolTip();
+
+            TipShow.AutoPopDelay = 10000;
+            TipShow.InitialDelay = 2000;
+            TipShow.ReshowDelay = 1000;
+            TipShow.ShowAlways = true;
+
+            TipShow.SetToolTip(this.TextBox_FilePath, this.TextBox_FilePath.Text);
+        }
+    }
+}
